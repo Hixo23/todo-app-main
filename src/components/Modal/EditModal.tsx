f511179d413d5d761c22de97543a962646ebd2ad
Hixo <hixohixo32@gmail.com>
2023-07-03 16:26:54 +0200

    Feat: add editing todos

---
+++ src/components/Modal/EditModal.tsx
@@ -0,0 +1,22 @@
+import { useContext, useState } from "react"
+import type { Todo } from "../../types/types"
+import { todosContext } from "../../contexts/todosContext"
+import { ThemeContext } from "../../contexts/themeContext"
+
+export const EditModal = ({name, id, handleEdit}: { name: string, id: number, handleEdit: (id: number, todoContent: string ) => void}) => {
+    const [todoContent, setTodoContent] = useState("")
+    const themeIsDark = useContext(ThemeContext);
+
+
+    return (
+        <div className="fixed flex justify-center items-center w-screen h-screen bg-black/[0.7] top-0 left-0">
+            <div className={`w-96  ${ themeIsDark ? "bg-slate-800 text-white" : "bg-gray-200 text-slate-800"} flex  flex-col px-4 rounded-xl`}>
+                <p className={`text-xl ${themeIsDark ? 'text-white': 'text-slate-800'} text-center mt-4`}>Edit Todo</p>
+               <form className="flex flex-col justify-between pb-20 gap-4" action="">
+               <input onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodoContent(e.target.value)} placeholder={name} className={`${ themeIsDark ? "bg-slate-700 text-white" : "bg-gray-200 text-slate-700"} rounded-lg md:outline-none p-4 w-full py-2 mt-4`} type="text" />
+                <button type="button" onClick={() => handleEdit(id, todoContent)} className={`py-2 px-5 rounded-lg ${themeIsDark ? 'bg-slate-700 text-white' : "bg-gray-200 text-slate-700"}`}>Edit</button>
+               </form>
+            </div>
+        </div>
+    )
+}
--- src/components/list/Item.tsx
+++ src/components/list/Item.tsx
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../contexts/themeContext";
 import { todosContext } from "../../contexts/todosContext";
 import { Draggable, DraggableProvided } from "react-beautiful-dnd";
+import { EditModal } from "../Modal/EditModal";
 
 interface Todo {
   name: string;
@@ -13,6 +14,7 @@ interface Todo {
 
 export const Item = (props: Todo) => {
   const { todos, setTodos } = useContext(todosContext);
+  const [modalIsOpen, setModalIsOpen] = useState(false)
 
   const handleDelete = (id: number) => {
     const newTodos = todos.filter(
@@ -23,9 +25,15 @@ export const Item = (props: Todo) => {
     setTodos(newTodos);
   };
 
+  const handleEdit = (id: number, todoContent: string) => {
+    setModalIsOpen(false);
+    return setTodos(todos.map(todo => todo.id === id ? {...todo, name: todoContent } : todo))
+}
+
   const theme = useContext(ThemeContext);
   return (
    <>
+      {modalIsOpen &&  <EditModal handleEdit={handleEdit} id={props.id} name={props.name} />}
     <Draggable draggableId={props.id.toString()} index={props.index}>
      {(provided: DraggableProvided) => (
        <li ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps} className={`border-b z-0 flex items-center justify-between py-3 px-6  ${
@@ -37,13 +45,17 @@ export const Item = (props: Todo) => {
         >
           {props.index + 1}. {props.name}
         </span>
+        <div className="flex gap-2 items-center">
         <button onClick={() => handleDelete(props.id)}>
           <span className="text-red-500 text-2xl">x</span>
         </button>
+        <button onClick={() => setModalIsOpen(true)}>
+        <i className="fa-solid fa-pen-to-square w-4 h-4"></i>
+        </button>
+        </div>
       </li>
      )}
     </Draggable>
-
     </>
   );
 };
